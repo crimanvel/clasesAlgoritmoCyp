a15516c96792d9c2ac9004220fde45659d1dde1a
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2024-09-10 17:14:48 +0200

    creando carpeta

---
+++ clase10-9-25.py
@@ -0,0 +1,37 @@
+#Tp1-Ejercicio 2
+#Leer dos números enteros y presentar por pantalla el prodemio de ellos.
+
+#Importación de librerias o archivos necesarios.
+
+#Entradas
+#num1=int(input("Ingrese un numero:"))
+#num2=int(input("Ingrese un numero:"))
+#Procesos
+#promedio = int(((num1+num2)/2))
+
+#Salidas
+#print(promedio)
+
+#TP1- Ejercicio 4 Convertir una medida dada en metros a sus equivalentes en decimetros y centimetros. Presentar por
+#pantalla las tres magnitudes con sus respectivas unidades.
+#entradas
+medidaMetros=float(input("Intrese la medida en metros: "))
+#procesos
+medidaDecimetros=medidaMetros*10
+medidaCentimetros=medidaMetros*100
+#salidas
+print("La medida en metros fue:",medidaMetros, "La medmida en decimetros es: ", medidaDecimetros, "la medida en centimetros es: ", medidaCentimetros)
+
+
+#tp1-Ejercicio 6
+#Ingresar los lados de un rectangulo, y presentar por pantalla su perímetro y su superficie.
+
+#Entradas
+#ladoUno=float(input("Ingrese el valor del lado 1: "))
+#ladoDos=float(input("Ingrese el valor del lado 2: "))
+#Procesos
+#perimetro =ladoUno+ladoDos
+#superficie=((ladoUno*ladoDos)/2)
+#Salidas
+#print("El perimetro del triangulo es:" ,perimetro)
+#print("La superficie del triangulo es:" ,superficie)
